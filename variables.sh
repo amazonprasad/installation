1a55b2d8e5ab31e24a8ff66bbba8064e26380544
muni <muni@gmail.com>
2025-01-31 14:55:14 +0530

    MM

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=prasad
+person2=muni
+
+echo " $person1: Hi $person2 how are you"
+echo " $person2: am good $person2. what about you"
+echo " $person1: am also good thank you for asking $person2"
+echo " what is the future plans $1person"
