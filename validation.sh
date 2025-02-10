18219cb1ba029f82f8097a0c93849224b83f5bb1
muni <muni@gmail.com>
2025-02-11 01:29:10 +0530

    Hl

--- validation.sh
+++ validation.sh
@@ -3,14 +3,17 @@ date=$(date +%F)
 USERID=$(id -u)
 scriptname=$0
 logfile=/tmp/$scriptname-$date.log
+r="\e[30m"
+g="\e[31m"
+n="\e[0m"
 
 validate(){
     if [ $1 -ne 0 ]
     then 
-        echo " $2.... failure"
+        echo " $2.... $r failure $n"
         exit 1
     else   
-        echo " $2 .... success"
+        echo " $2 .... $g success $n"
     fi
 }
 if [ $USERID -ne 0 ]
