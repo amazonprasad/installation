27d9269480da14683c99bd51b9491af9218b0e00
muni <muni@gmail.com>
2025-02-11 00:38:09 +0530

    Hl

--- validation.sh
+++ validation.sh
@@ -4,9 +4,17 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "ERROR: Your not the root user"
+    exit 1
 else
     echo " Your are the root user"
 fi
 
-    yum install mysql -y 
-    
+yum install mysql -y 
+
+if [ $? -ne o ]
+then 
+    echo " installation of mysql is error"
+    exit 1
+else
+    echo " installation of mysql is success"
+fi
