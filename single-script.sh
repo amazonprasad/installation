0cf80bc12ec922f174fad23ccbc374f23fa05a5a
muni <muni@gmail.com>
2025-02-12 17:25:15 +0530

    Hl

---
+++ single-script.sh
@@ -0,0 +1,34 @@
+ #!/bin/bash
+ DATE=$(date +%F)
+ USERID=$(id -u)
+ R="\e[31m"
+ G="\e[32m"
+ N="\e[30m"
+ VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e " Installing...$R is failure $N"
+        exit 1
+    else
+        echo -e " Installing...$G is success $N"
+    fi
+ }
+if [ $USERID -ne 0 ]
+then 
+    echo -e " $R Error: Your not the root user $N"
+else 
+    echo -e " $G Your are the root user $N"
+fi 
+
+for i in $@
+do 
+    yum list install $! 
+    if [ $? -ne 0 ]
+    then 
+        echo -e "  $R $i is not installed, let us install now $N"
+        yum install $i -y
+        VALIDATE $? "$i"
+    else 
+        echo -e " $G $i is already installed $N"
+    fi 
+done
